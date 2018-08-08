a85ba04dd7091f45dce88d13debaf1bd4e98e8bc
Unknown <Christian.Fischer1@hs-augsburg.de>
2018-08-08 21:37:29 +0200

    yoyooyo error

--- src/apollo/resolvers.ts
+++ src/apollo/resolvers.ts
@@ -20,6 +20,41 @@ export const defaults = {
   gameField: addTypedefToData(initialize(8, 8)),
 };
 
+const djWrapper = (gameField: IGameField) => {
+  const clicks = [];
+  let currentGameField = gameField;
+  let won = false;
+  while (!won) {
+    const shortest = djShit(currentGameField);
+    clicks.push(shortest.idx);
+    if (shortest.pathLength === 0) {
+      won = true;
+    }
+  }
+};
+
+const dj = (gameField: IGameField) => {};
+
+const djShit = (gameField: IGameField) => {
+  const shortestPaths = gameField.cells.map(cell => {
+    const newGameField = reducer(cell.id, gameField);
+    return {
+      pathLength: dj(newGameField),
+      gameField: newGameField,
+      idx: cell.id,
+    };
+  });
+  return shortestPaths.reduce(
+    (old, obj) => {
+      if (obj.pathLength < old.pathLength) {
+        return obj;
+      }
+      return old;
+    },
+    { pathLength: Infinity, idx: -1, gameField: gameField },
+  );
+};
+
 export const resolvers = {
   Mutation: {
     callCell: (
