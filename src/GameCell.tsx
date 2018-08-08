587f7b77f1b9c3d4fec59e74ae6efa63ec8b80da
zoidburger <roman@salameh.de>
2018-08-08 18:34:24 +0200

    ugly colors

--- src/GameCell.tsx
+++ src/GameCell.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import {GameCellInterface} from "./types";
+import { GameCellInterface } from "./types";
 
 
 const CellStyle = styled.div`
@@ -17,20 +17,20 @@ const CellStyle = styled.div`
     color:red;
     border: 2px solid red;
   }
-  background-color: ${(props: {bgColor: string}) => props.bgColor};
+  background-color: ${(props: { bgColor: string }) => props.bgColor};
 `;
 
 
-const colorScaled = ({value}:{value: number}) => {
-        //value from 0 to 1
-        var hue=((1-(value/8))*120).toString(10);
-        return ["hsl(",hue,",96%,69%)"].join("");
+const colorScaled = (value: number) => {
+    //value from 0 to 1
+    var hue = ((1 - (value / 8)) * 120).toString(10);
+    return ["hsl(", hue, ",96%,69%)"].join("");
 };
 
-export function GameCell({props, fn}:{props:GameCellInterface, fn: ()=>void}) {
-    return(
-        <CellStyle 
-            bgColor={colorScaled(props)}
+export function GameCell({ props, fn }: { props: any, fn: () => void }) {
+    return (
+        <CellStyle
+            bgColor={colorScaled(props.value)}
             onClick={fn}>
             {props.value}
         </CellStyle>
